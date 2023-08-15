78ac02e28e3dc190c7051e18f8ebc93a3791d0e6
mrzogy <magbolmrzogy@gmail.com>
2023-08-15 22:30:53 +0300

    Initial Commit

---
+++ FoodDesaign/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FoodDesaign
+//
+//  Created by 3bood on 28/01/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodDesaign/FoodDesaignApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDesaignApp.swift
+//  FoodDesaign
+//
+//  Created by 3bood on 28/01/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct FoodDesaignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
